3aab6e623c0aa90363a3955d2dfa735345aea8fc
Andreas Giannakopoulos <gianandr4@gmail.com>
2021-09-17 14:39:58 +0300

    Initial commit

---
+++ create_json.py
@@ -0,0 +1,16 @@
+import json, requests
+
+
+repos=['https://raw.githubusercontent.com/portainer/templates/master/templates-2.0.json',
+       'https://raw.githubusercontent.com/Qballjos/portainer_templates/master/Template/template.json',
+       'https://raw.githubusercontent.com/dnburgess/dbtechtemplate/master/Template/v2/templates.json']
+
+
+final_repo = {'version': '2','templates':[]}
+
+for repo in repos:
+    dict = json.loads(requests.get(repo).text)
+    final_repo['templates'].extend(dict['templates'])
+
+with open('portainer.json','w') as f:
+    json.dumps(final_repo,f)
